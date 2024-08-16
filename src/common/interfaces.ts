9a6b3066ea508e92a4dc2fae6b5fd97124ec557b
Aryamanraj <20cs3011@rgipt.ac.in>
2024-08-16 16:01:55 +0530

    feat: added event listner for PONG and NEW_PINGER

--- src/common/interfaces.ts
+++ src/common/interfaces.ts
@@ -1,3 +1,5 @@
+import { ethers, Log as ethersLog, LogDescription } from 'ethers';
+
 export interface ResultWithError {
   error: any;
   data: any;
@@ -14,4 +16,70 @@ export type JSONArray = Array<JSONValue>;
 export interface BASE_EVENT_DATA {
   txHash: string;
   timestamp: number;
+}
+
+export interface NEW_PINGER_EVENT_DATA extends BASE_EVENT_DATA {
+  pinger: ethers.Addressable;
+  blockNumber: number;
+  logIndex: number;
+}
+
+export interface PONG_EVENT_DATA extends BASE_EVENT_DATA {
+  originalTxHash: string;
+  blockNumber: number;
+  logIndex: number;
+}
+
+interface Log {
+  _type: string;
+  address: string;
+  blockHash: string;
+  blockNumber: number;
+  data: string;
+  index: number;
+  removed: boolean;
+  topics: string[];
+  transactionHash: string;
+  transactionIndex: number;
+}
+
+interface Fragment {
+  type: string;
+  inputs: any[]; 
+  name: string;
+  anonymous?: boolean;
+}
+
+interface Emitter {
+  target: string;
+  interface: {
+    fragments: Fragment[];
+    deploy: Fragment;
+    fallback: null | any;
+    receive: boolean;
+  };
+  runner: {
+    provider: any; 
+    address: string;
+  };
+  filters: any;
+  fallback: null | any;
+}
+
+export interface PingEvent {
+  filter: string;
+  emitter: Emitter;
+  log: Log;
+  args: any[];
+  fragment: Fragment;
+}
+
+export interface ParsedLog {
+  log: ethersLog;
+  parsedLog: LogDescription;
+}
+
+export interface GetNewTransactionsResult {
+  parsedLogArray: ParsedLog[];
+  toBlockNumber: number;
 }
--- src/rpc/rpc.service.ts
+++ src/rpc/rpc.service.ts
@@ -22,8 +22,10 @@ import { ONCHAIN_CONFIG } from '../common/web3';
 import {
   BASE_EVENT_DATA,
   GetNewTransactionsResult,
+  NEW_PINGER_EVENT_DATA,
   ParsedLog,
   PingEvent,
+  PONG_EVENT_DATA,
   ResultWithError,
 } from '../common/interfaces';
 import PingPongABI from './abi/PingPong.json';
@@ -126,6 +128,7 @@ export class RpcService implements OnApplicationBootstrap {
         //     },
         //   },
         // );
+
         this.logger.info(`Added new join challenge job [jobId :]`);
       } catch (error) {
         this.logger.error(
@@ -135,6 +138,110 @@ export class RpcService implements OnApplicationBootstrap {
         );
       }
     });
+
+    // NewPinger event listener
+    this.contract.on(EventTypes.NEW_PINGER, async (event) => {
+      try {
+        const newPingerAddress = event?.pinger as ethers.Addressable;
+        const txHash = event?.log?.transactionHash as string;
+        const blockNumber = event?.log?.blockNumber as number;
+        const logIndex = event?.log?.logIndex as number;
+        if (!newPingerAddress || !txHash) {
+          throw new Error(
+            'Pinger address or transaction hash is missing from the event.',
+          );
+        }
+
+        this.logger.info(
+          `Received NewPinger event with pinger: ${newPingerAddress}, txHash: ${txHash}`,
+        );
+
+        // TODO: Implement functionality to handle the change in pinger address
+
+        const eventData: NEW_PINGER_EVENT_DATA = {
+          txHash,
+          pinger: newPingerAddress,
+          timestamp: Date.now(),
+          blockNumber,
+          logIndex,
+        };
+
+        // const { id: jobId } = await this.logsQueue.add(
+        //   QUEUE_JOB_NAMES.PONG_TRANSACTION,
+        //   {
+        //     data: eventData,
+        //   },
+        //   {
+        //     attempts: 3, // retry 3 times max
+        //     backoff: {
+        //       type: 'exponential', // exponential backoff strategy
+        //       delay: 1000, // initial delay of 1s, increasing exponentially
+        //     },
+        //   },
+        // );
+
+        this.logger.info(
+          `Processed NewPinger event with pinger: ${newPingerAddress}`,
+        );
+      } catch (error) {
+        this.logger.error(
+          `Error processing NewPinger event: ${JSON.stringify(event)} : ${
+            error.stack
+          }`,
+        );
+      }
+    });
+
+    // Pong event listener
+    this.contract.on(EventTypes.PONG, async (event) => {
+      try {
+        const txHash = event?.log?.transactionHash;
+        const blockNumber = event?.log?.blockNumber;
+        const logIndex = event?.log?.logIndex;
+        const originalTxHash = event?.args?.txHash; 
+        if (!txHash || !originalTxHash) {
+          throw new Error(
+            'Transaction hash or original txHash is missing from the event.',
+          );
+        }
+
+        this.logger.info(
+          `Received Pong event with txHash: ${txHash}, originalTxHash: ${originalTxHash}`,
+        );
+
+        // TODO: Implement functionality to handle the Pong event
+        const eventData: PONG_EVENT_DATA = {
+          txHash,
+          originalTxHash,
+          timestamp: Date.now(),
+          blockNumber,
+          logIndex,
+        };
+
+        // const { id: jobId } = await this.logsQueue.add(
+        //   QUEUE_JOB_NAMES.PONG_TRANSACTION,
+        //   {
+        //     data: eventData,
+        //   },
+        //   {
+        //     attempts: 3, // retry 3 times max
+        //     backoff: {
+        //       type: 'exponential', // exponential backoff strategy
+        //       delay: 1000, // initial delay of 1s, increasing exponentially
+        //     },
+        //   },
+        // );
+
+        this.logger.info(`Processed Pong event with txHash: ${txHash}`);
+      } catch (error) {
+        this.logger.error(
+          `Error processing Pong event with txHash: ${JSON.stringify(
+            event,
+          )} : ${error.stack}`,
+        );
+      }
+    });
+
     this.logger.info(
       `Listening to [events : [${Object.values(
         EventTypes,
@@ -169,8 +276,15 @@ export class RpcService implements OnApplicationBootstrap {
         parsedLogArray: parsedLogs,
         toBlockNumber: toBlock - 1,
       };
-
-      console.log(result);
+      if (parsedLogs.length > 0) {
+        this.logger.info(
+          `Found log for ${
+            this.contractAddress
+          } finding from block: ${fromBlock} to block: ${toBlock} log result: ${JSON.stringify(
+            result,
+          )}`,
+        );
+      }
       return { data: result, error: null };
     } catch (error) {
       this.logger.error(`Error fetching new transactions: ${error.stack}`);
