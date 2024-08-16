fc41ca459f30dc6a3696b95ea77cf5647a0d4a33
Aryamanraj <20cs3011@rgipt.ac.in>
2024-08-16 15:35:22 +0530

    feat: health api and index state dto

---
+++ src/admin/dto/indexed-state.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class IndexedStateDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  network: string;
+
+  @ApiProperty()
+  @IsOptional()
+  blockNumber?: number;
+}
